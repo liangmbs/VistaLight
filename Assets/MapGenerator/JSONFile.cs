95c4ae994bcf721ec2d2e2eee9a6a66425a4c794
ChiSheng Liang <chishengliang@ChiShengs-MacBook-Pro.local>
2015-10-01 11:33:25 -0400

    editing the map genrator

--- Assets/MapGenerator/JSONFile.cs
+++ Assets/MapGenerator/JSONFile.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
+using System.IO;
 
 public class JSONFile : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		SimpleJSON.JSONNode node = SimpleJSON.JSONNode.Parse (Resources.Load<TextAsset>);
+
+		File.WriteAllText(@"/Users/chishengliang/Documents/VistaLight/Assets/test", JSON);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
