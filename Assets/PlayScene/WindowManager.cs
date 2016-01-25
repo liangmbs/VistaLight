c239c564a47256bf8ee8b5533074cbd1c3e20c0e
liangmbs <chishengliang@ChiShengs-MacBook-Pro.local>
2016-01-25 14:43:00 -0500

    editing playing scene

---
+++ Assets/PlayScene/WindowManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using SimpleJSON;
+using UnityEngine.UI;
+using System.Collections.Generic; 
+
+public class WindowManager: MonoBehaviour {
+
+	public Button toggleButton;
+	public GameObject infoBar;
+	public bool open = false;
+
+	public void ToggleBottomInformationPanel(){
+		open = !open;
+		if (open) {
+			infoBar.GetComponent<RectTransform> ().localPosition= new Vector3(0,0,0);
+		}
+	}
+
+
+}
+		
