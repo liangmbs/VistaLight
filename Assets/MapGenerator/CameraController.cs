c8bc4b032537fa3f79c8447e3582132ca3fcd18a
Yifan Sun <sunyifan112358@gmail.com>
2016-01-24 22:05:15 -0500

    Move camera with mouse middle button and WASD key.

--- Assets/MapGenerator/CameraController.cs
+++ Assets/MapGenerator/CameraController.cs
@@ -4,6 +4,8 @@ using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour {
 
+	public double sensitivity = 30;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +16,9 @@ public class CameraController : MonoBehaviour {
 		float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
 		ZoomInAndOut(mouseWheel);
 
+		MoveWithMouseMiddleButton();
+		MoveWithWASD();
+
 		double widthPercent = ((double)Screen.width - 200.0) / (double)Screen.width;
 		double heightPercent = ((double)Screen.height - 80.0) / (double)Screen.height;
 		gameObject.GetComponent<Camera>().rect = new Rect(
@@ -22,8 +27,34 @@ public class CameraController : MonoBehaviour {
 			(float)heightPercent);
 	}
 
-	void ZoomInAndOut(float mouseWheel) {
+	void MoveWithMouseMiddleButton() {
+		if (Input.GetMouseButton(2)) {
+			double cameraSize = gameObject.GetComponent<Camera>().orthographicSize;
+			transform.position += new Vector3(
+				(float)(-Input.GetAxis("Mouse X") * cameraSize / sensitivity),
+				(float)(-Input.GetAxis("Mouse Y") * cameraSize / sensitivity), 0);
+		}
+	}
 
+	void MoveWithWASD() {
+		double cameraSize = gameObject.GetComponent<Camera>().orthographicSize;
+		double moveLength = cameraSize / sensitivity;
+		if (Input.GetKey("w")) {
+			transform.position += new Vector3(
+				0, (float)(moveLength), 0);
+		} else if (Input.GetKey("a")) {
+			transform.position += new Vector3(
+				(float)(-moveLength), 0, 0);
+		} else if (Input.GetKey("s")) {
+			transform.position += new Vector3(
+				0, (float)(-moveLength), 0);
+		} else if (Input.GetKey("d")) {
+			transform.position += new Vector3(
+				(float)(moveLength), 0, 0);
+		}
+	}
+
+	void ZoomInAndOut(float mouseWheel) {
 		// If over gui, do not zoom
 		if (EventSystem.current.IsPointerOverGameObject()) {
 			return;
