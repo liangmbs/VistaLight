a4cf67ade4d6f5a689717aadb6870f33f3669544
Yifan Sun <sunyifan112358@gmail.com>
2016-01-26 13:26:07 -0500

    Serialize ship generation event and oil spilling event

--- Assets/Map/Event/OilSpillingEvent.cs
+++ Assets/Map/Event/OilSpillingEvent.cs
@@ -1,6 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.Serialization;
+using System;
 
+[Serializable()]
 public class OilSpillingEvent : MapEvent {
 
+	public OilSpillingEvent() { 
+	}
+
+	// Serialization
+	public override void GetObjectData(SerializationInfo info, StreamingContext context) {
+		base.GetObjectData(info, context);
+	}
+
+	// Deserialiation
+	public OilSpillingEvent(SerializationInfo info, StreamingContext context) : 
+		base(info, context) {
+		Debug.Log("oil spilling event.");
+	}
 }
--- Assets/Map/Event/ShipGenerationEvent.cs
+++ Assets/Map/Event/ShipGenerationEvent.cs
@@ -3,5 +3,20 @@ using System.Collections;
 using System;
 using System.Runtime.Serialization;
 
+[Serializable()]
 public class ShipGenerationEvent : MapEvent {
+
+	public ShipGenerationEvent() { 
+	}
+
+	// Serialization
+	public override void GetObjectData(SerializationInfo info, StreamingContext context) {
+		base.GetObjectData(info, context);
+	}
+
+	// Deserialiation
+	public ShipGenerationEvent(SerializationInfo info, StreamingContext context) : 
+		base(info, context) {
+		Debug.Log("ship generation event.");
+	}
 }
