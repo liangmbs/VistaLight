990da4dbf2bfd55d2b54857fb517a2be8b8abb9b
Yifan Sun <sunyifan112358@gmail.com>
2016-03-15 21:43:27 -0400

    Improve dynamic clock

--- Assets/HostScene/ClockVO.cs
+++ Assets/HostScene/ClockVO.cs
@@ -19,8 +19,8 @@ public class ClockVO : MonoBehaviour {
 		int minute = Timer.VirtualTime.Minute;
 		int second = Timer.VirtualTime.Second;
 
-		float hourAngle = (float) (-(hour * 30.0 + minute / 2.0 + second * 30.0 / 3600.0) + 90);
-		float minuteAngle = (float)(-(minute * 6.0 + second / 10.0) + 90);
+		float hourAngle = -(float) (hour * 30.0 + minute / 2.0 + second * 30.0 / 3600.0);
+		float minuteAngle = -(float) (minute * 6.0 + second / 10.0);
 
 		HourHand.gameObject.GetComponent<RectTransform>().eulerAngles = new Vector3 (0, 0, hourAngle);
 		MinuteHand.gameObject.GetComponent<RectTransform>().eulerAngles = new Vector3 (0, 0, minuteAngle);
