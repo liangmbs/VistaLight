0876185729d7b3b7a5809c4b761a49c293d0cf46
Yifan Sun <sunyifan112358@gmail.com>
2016-03-15 21:21:56 -0400

    Dynamic clock

---
+++ Assets/HostScene/ClockVO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClockVO : MonoBehaviour {
+
+	public Timer Timer;
+	public Image HourHand;
+	public Image MinuteHand;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int hour = Timer.VirtualTime.Hour;
+		int minute = Timer.VirtualTime.Minute;
+		int second = Timer.VirtualTime.Second;
+
+		float hourAngle = (float) (-(hour * 30.0 + minute / 2.0 + second * 30.0 / 3600.0) + 90);
+		float minuteAngle = (float)(-(minute * 6.0 + second / 10.0) + 90);
+
+		HourHand.gameObject.GetComponent<RectTransform>().eulerAngles = new Vector3 (0, 0, hourAngle);
+		MinuteHand.gameObject.GetComponent<RectTransform>().eulerAngles = new Vector3 (0, 0, minuteAngle);
+	}
+}
