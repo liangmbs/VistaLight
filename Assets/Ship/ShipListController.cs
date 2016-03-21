2f7a4007fd1a5f6b92603a7d237fe1ba81f3d569
Yifan Sun <sunyifan112358@gmail.com>
2016-03-20 20:56:14 -0400

    Add oid priority field

--- Assets/Ship/ShipListController.cs
+++ Assets/Ship/ShipListController.cs
@@ -6,6 +6,8 @@ public class ShipListController : MonoBehaviour {
 
 	public GameObject entryPrefab;
 	public List<ShipListEntryController> entries = new List<ShipListEntryController>();
+	public GameObject listHeader;
+	public GameObject headerOldPriority;
 
 	// Use this for initialization
 	void Start () {
@@ -55,6 +57,24 @@ public class ShipListController : MonoBehaviour {
 		RectTransform rectTransform = gameObject.GetComponent<RectTransform> ();
 		Vector2 sizeDelta = rectTransform.sizeDelta;
 		rectTransform.sizeDelta = new Vector2 (sizeDelta.x + 40, sizeDelta.y);
+
+		ShowOldPriorityInHeader ();
+
+	}
+
+	void ShowOldPriorityInHeader ()
+	{
+		headerOldPriority.SetActive (true);
+		listHeader.transform.Find ("Status").Translate (new Vector3 (40, 0, 0));
+		listHeader.transform.Find ("Name").Translate (new Vector3 (40, 0, 0));
+		listHeader.transform.Find ("Type").Translate (new Vector3 (40, 0, 0));
+		listHeader.transform.Find ("Amount").Translate (new Vector3 (40, 0, 0));
+		listHeader.transform.Find ("Unit Value").Translate (new Vector3 (40, 0, 0));
+		listHeader.transform.Find ("Due Time").Translate (new Vector3 (40, 0, 0));
+		listHeader.transform.Find ("ETA").Translate (new Vector3 (40, 0, 0));
+		RectTransform rectTransform = listHeader.GetComponent<RectTransform> ();
+		Vector2 sizeDelta = rectTransform.sizeDelta;
+		rectTransform.sizeDelta = new Vector2 (sizeDelta.x + 40, sizeDelta.y);
 	}
 
 	public void HideNewPriority() {
@@ -63,6 +83,24 @@ public class ShipListController : MonoBehaviour {
 		}
 
 		RectTransform rectTransform = gameObject.GetComponent<RectTransform> ();
+		Vector2 sizeDelta = rectTransform.sizeDelta;
+		rectTransform.sizeDelta = new Vector2 (sizeDelta.x - 40, sizeDelta.y);
+
+		HideOldPriorityInHeader ();
+
+	}
+
+	void HideOldPriorityInHeader ()
+	{
+		headerOldPriority.SetActive (false);
+		listHeader.transform.Find ("Status").Translate (new Vector3 (-40, 0, 0));
+		listHeader.transform.Find ("Name").Translate (new Vector3 (-40, 0, 0));
+		listHeader.transform.Find ("Type").Translate (new Vector3 (-40, 0, 0));
+		listHeader.transform.Find ("Amount").Translate (new Vector3 (-40, 0, 0));
+		listHeader.transform.Find ("Unit Value").Translate (new Vector3 (-40, 0, 0));
+		listHeader.transform.Find ("Due Time").Translate (new Vector3 (-40, 0, 0));
+		listHeader.transform.Find ("ETA").Translate (new Vector3 (-40, 0, 0));
+		RectTransform rectTransform = listHeader.GetComponent<RectTransform> ();
 		Vector2 sizeDelta = rectTransform.sizeDelta;
 		rectTransform.sizeDelta = new Vector2 (sizeDelta.x - 40, sizeDelta.y);
 	}
